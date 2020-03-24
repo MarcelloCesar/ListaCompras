cff4af977af28706966ea60763e5a815e0783749
Marcello Cesar Alves de Souza Queiroz <Marcellocesar.queiroz@gmail.com>
2020-03-23 21:26:07 -0300

    Projeto nativo

--- app/src/main/java/com/example/listacompras/MainActivity.kt
+++ app/src/main/java/com/example/listacompras/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.listacompras
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     var itensAdicionados = "";
+    var contView = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     fun btnAddClick(view: View) {
         var nomeProduto = findViewById<TextView>(R.id.productName)
         var qtde = findViewById<TextView>(R.id.qtde)
+        var ll = findViewById<LinearLayout>(R.id.linearLayout)
 
         if(itensAdicionados == ""){
             itensAdicionados = itensAdicionados +  qtde.getText() + " x " + nomeProduto.getText()
@@ -27,8 +31,30 @@ class MainActivity : AppCompatActivity() {
             itensAdicionados = itensAdicionados + ", " +  qtde.getText() + " x " + nomeProduto.getText()
         }
 
-        findViewById<TextView>(R.id.teste).setText(itensAdicionados)
+        var newLabel = TextView(this)
+        newLabel.text = "" + qtde.getText() + " x " + nomeProduto.getText()
 
+        var botao = Button(this)
+        botao.text = "Remover"
+
+        botao.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View) {
+                ll.removeView(v.parent as View)
+            }
+        })
+
+        var container = LinearLayout(this)
+        container.orientation = LinearLayout.HORIZONTAL
+
+        var separador = TextView(this)
+        separador.text = "          "
+
+
+        container.addView(botao)
+        container.addView(separador)
+        container.addView(newLabel)
+
+        ll.addView(container)
 
     }
 }
